d1f5ed7246b8e2a9fd8433a678113b015dd4ae7b
moistvonlipwig <chris.teli@gmail.com>
2014-04-01 15:09:28 -0700

    adding acts_as_commentable data

---
+++ dodolist/app/controllers/comments_controller.rb
@@ -0,0 +1,14 @@
+# comments_controller.rb
+class CommentsController < ApplicationController
+  def create
+    @comment_hash = params[:comment]
+    @obj = @comment_hash[:commentable_type].constantize.find(@comment_hash[:commentable_id])
+    # Not implemented: check to see whether the user has permission to create a comment on this object
+    @comment = Comment.build_from(@obj, current_user, @comment_hash[:body])
+    if @comment.save
+      render :partial => "comments/comment", :locals => { :comment => @comment }, :layout => false, :status => :created
+    else
+      render :js => "alert('error saving comment');"
+    end
+  end
+end
