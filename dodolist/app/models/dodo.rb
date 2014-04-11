56a346ef8da55a8eb18da67f6d5532585f8b45a7
moistvonlipwig <chris.teli@gmail.com>
2014-04-11 09:56:25 -0700

    Added links to model

--- dodolist/app/models/dodo.rb
+++ dodolist/app/models/dodo.rb
@@ -1,4 +1,5 @@
 class Dodo < ActiveRecord::Base
+  has_many :links
   validates :name, :presence => true	
   acts_as_taggable
   acts_as_commentable
--- dodolist/db/schema.rb
+++ dodolist/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140401204910) do
+ActiveRecord::Schema.define(version: 20140411162634) do
 
   create_table "comments", force: true do |t|
     t.integer  "commentable_id",   default: 0
@@ -36,6 +36,9 @@ ActiveRecord::Schema.define(version: 20140401204910) do
     t.datetime "updated_at"
     t.boolean  "complete"
   end
+
+# Could not dump table "links" because of following NoMethodError
+#   undefined method `[]' for nil:NilClass
 
   create_table "taggings", force: true do |t|
     t.integer  "tag_id"
